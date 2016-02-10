bbbcccfaaa01e26ef2f02b4d78c6a4d82ec62bb6
nmjmdr <narasimha.gm@gmail.com>
2016-02-10 09:57:49 +0000

    Formatted code

--- src/chained/chained_test.go
+++ src/chained/chained_test.go
@@ -1,11 +1,11 @@
 package chained
 
 import (
+	"hash/fnv"
+	"strconv"
+	"sync"
 	"testing"
 	"unsafe"
-	"hash/fnv"	
-	"sync"
-	"strconv"
 )
 
 type mockValueFn func() uint64
@@ -24,7 +24,6 @@ func (m *mockHash64) Sum(b []byte) []byte {
 func (m *mockHash64) Reset() {
 }
 
- 
 func (m *mockHash64) Size() int {
 	return 0
 }
@@ -34,7 +33,7 @@ func (m *mockHash64) BlockSize() int {
 }
 
 func (m *mockHash64) Write(p []byte) (n int, err error) {
-	return 0,nil
+	return 0, nil
 }
 
 func newMockHash(c mockValueFn) *mockHash64 {
@@ -43,42 +42,36 @@ func newMockHash(c mockValueFn) *mockHash64 {
 	return mock
 }
 
-
 func TestSizeNode(t *testing.T) {
-	
-	c := NewChainedHash(1,fnv.New64a())
 
-	size := unsafe.Sizeof(c.arr[0])	
+	c := NewChainedHash(1, fnv.New64a())
+
+	size := unsafe.Sizeof(c.arr[0])
 	t.Log(size)
 
-	
 }
 
-
 func TestSizeExt(t *testing.T) {
-	
+
 	var e ext
 	size := unsafe.Sizeof(e)
 	t.Log(size)
-	
-}
 
+}
 
 func TestAdd_IndexMap(t *testing.T) {
 
-
-	fn := func() uint64 { 
+	fn := func() uint64 {
 		return uint64(1)
 	}
 	c := newMockHash(fn)
-	h := NewChainedHash(2,c)
+	h := NewChainedHash(2, c)
 
-	h.coder = newMockHash( func() uint64 { return 1 })
-	h.Put("a","a")
+	h.coder = newMockHash(func() uint64 { return 1 })
+	h.Put("a", "a")
 
-	
-	h.coder = newMockHash( func() uint64 { return 2 })
-	h.Put("b","b")
+	h.coder = newMockHash(func() uint64 { return 2 })
+	h.Put("b", "b")
 
 	if h.arr[1].entry == nil || h.arr[1].entry.value != "a" {
 		t.Fatal("value does not match for key: a")
@@ -90,80 +83,73 @@ func TestAdd_IndexMap(t *testing.T) {
 
 }
 
-
 func TestAdd_NodeNextSet(t *testing.T) {
 
-
-	fn := func() uint64 { 
+	fn := func() uint64 {
 		return uint64(1)
 	}
 	c := newMockHash(fn)
-	h := NewChainedHash(2,c)
+	h := NewChainedHash(2, c)
 
-	h.coder = newMockHash( func() uint64 { return 1 })
-	h.Put("a","a")
+	h.coder = newMockHash(func() uint64 { return 1 })
+	h.Put("a", "a")
 
-	
-	h.coder = newMockHash( func() uint64 { return 1 })
-	h.Put("b","b")
+	h.coder = newMockHash(func() uint64 { return 1 })
+	h.Put("b", "b")
 
 	if h.arr[1].next == nil {
 		t.Fatal("value does not match for key: next is nil")
-	}else if h.arr[1].next.entry[0] == nil {
+	} else if h.arr[1].next.entry[0] == nil {
 		t.Fatal("value does not match for key: next.entry is nil")
-	}else if h.arr[1].next.entry[0].value != "b" {
+	} else if h.arr[1].next.entry[0].value != "b" {
 		t.Fatal("value does not match for key: b")
 	}
 
 }
 
-
 func TestAdd_ExtIndexSet(t *testing.T) {
 
-
-	fn := func() uint64 { 
+	fn := func() uint64 {
 		return uint64(1)
 	}
 	c := newMockHash(fn)
-	h := NewChainedHash(2,c)
+	h := NewChainedHash(2, c)
 
-	h.coder = newMockHash( func() uint64 { return 1 })
-	h.Put("a","a")
-	h.Put("b","b") //0
-	h.Put("c","c") //1
-	h.Put("d","d") //2
+	h.coder = newMockHash(func() uint64 { return 1 })
+	h.Put("a", "a")
+	h.Put("b", "b") //0
+	h.Put("c", "c") //1
+	h.Put("d", "d") //2
 
 	if h.arr[1].next == nil {
 		t.Fatal("value does not match for key: next is nil")
-	}else if h.arr[1].next.entry[2] == nil {
+	} else if h.arr[1].next.entry[2] == nil {
 		t.Fatal("value does not match for key: next.entry is nil")
-	}else if h.arr[1].next.entry[2].value != "d" {
+	} else if h.arr[1].next.entry[2].value != "d" {
 		t.Fatal("value does not match for key: d")
 	}
 
 }
 
-
-
 func TestAdd_NextSetParallel(t *testing.T) {
 
-	fn := func() uint64 { 
+	fn := func() uint64 {
 		return uint64(1)
 	}
 	c := newMockHash(fn)
-	h := NewChainedHash(2,c)
+	h := NewChainedHash(2, c)
 
 	done := make(chan bool)
 	running := make(chan bool)
 	n := 10
-	
+
 	var m sync.Mutex
- 	cond := sync.NewCond(&m)
+	cond := sync.NewCond(&m)
 	allset := false
 
-	for i:=0;i<n;i++ {
-		go func() {	
-			
+	for i := 0; i < n; i++ {
+		go func() {
+
 			cond.L.Lock()
 			running <- true
 			for !allset {
@@ -171,16 +157,15 @@ func TestAdd_NextSetParallel(t *testing.T) {
 			}
 			cond.L.Unlock()
 
-		
 			h.coder = newMockHash(fn)
-			h.Put(strconv.Itoa(i),strconv.Itoa(i))
+			h.Put(strconv.Itoa(i), strconv.Itoa(i))
 			done <- true
-			
-		}()			
-	}	
+
+		}()
+	}
 
 	// wait for everything to start running
-	for i:=0;i<n;i++ {
+	for i := 0; i < n; i++ {
 		<-running
 		if i == n-1 {
 			cond.L.Lock()
@@ -188,25 +173,22 @@ func TestAdd_NextSetParallel(t *testing.T) {
 			cond.L.Unlock()
 		}
 	}
-	
+
 	// now signal all go routines to proceed with their work
 	cond.L.Lock()
 	cond.Broadcast()
 	cond.L.Unlock()
-	
+
 	// wait for everyting to finish
-	for i:=0;i<n;i++ {
-		<-done		
+	for i := 0; i < n; i++ {
+		<-done
 	}
 
 	counter := 1
-	for p := h.arr[1].next; p!=nil; p = p.next {
+	for p := h.arr[1].next; p != nil; p = p.next {
 		counter += 3
-	}	
+	}
 	if counter != n {
 		t.Fail()
 	}
 }
-
-
-
